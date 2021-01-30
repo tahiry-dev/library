953768385e2a150482db998f2d94a8a259899d72
tahiry-dev <tahirycontact@gmail.com>
2021-01-30 18:52:22 +0300

    allo user to add book information

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 let myLibrary = []
+let i = 0;
 
-function Book(author, title, pages) {
+function Book(author, title, page) {
     this.author = author
     this.title = title
-    this.pages = pages
+    this.page = page
 }
 
-
-Book.prototype.addBookToLibrary = function () {
-
-    let book_obj = {
-        author: this.author,
-        title: this.title,
-        pages: this.pages
-    }
-
-    myLibrary.push(book_obj)
+function getObject() {
+    myLibrary[i] = new Book(document.getElementById("author").value, document.getElementById("title").value, document.getElementById("page").value)
+    i++;
+    document.getElementById("author").value = "";
+    document.getElementById("title").value = "";
+    document.getElementById("page").value = "";
 }
 
+function display_object() {
+    let e = " ";
+    let target = document.getElementById("list-container");
+    myLibrary.forEach(element => {
+        for (let keys in element) {
+            e += (`<p><b>${keys} :</b> ${element[keys]} </p>`);
+        }
 
+    })
+
+    target.innerHTML = e;
+}
