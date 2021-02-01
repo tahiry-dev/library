88bc1101675c294cc9a2952226b6a0085bf2fba7
tahiry-dev <tahirycontact@gmail.com>
2021-02-01 13:58:04 +0300

    fix eslit errors

--- app.js
+++ app.js
@@ -2,25 +2,26 @@ let myLibrary = [];
 let id = 1;
 
 function Book(author, title, page, id) {
-    this.author = author;
-    this.title = title;
-    this.page = page;
-    this.id = id;
+  this.author = author;
+  this.title = title;
+  this.page = page;
+  this.id = id;
 }
 
-function addBook() {
-    const book = new Book(document.getElementById("author").value, document.getElementById("title").value, document.getElementById("page").value, id);
-    id++;
-    myLibrary.push(book);
-    document.getElementById("author").value = "";
-    document.getElementById("title").value = "";
-    document.getElementById("page").value = "";
+function addBook() { // eslint-disable-line no-unused-vars
+  const book = new Book(document.getElementById('author').value, document.getElementById('title').value, document.getElementById('page').value, id);
+  id += 1;
+  myLibrary.push(book);
+  document.getElementById('author').value = '';
+  document.getElementById('title').value = '';
+  document.getElementById('page').value = ''; // still raises defined but never used
 }
 
 function displayBooks() {
-    let e = '';
-    for (let i = 0; i < myLibrary.length; i++) {
-        e += `
+  let e = '';
+
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    e += `
           <article class="book-card">
             <p><b>Author</b>: ${myLibrary[i].author}</p>
             <p><b>Title</b>: ${myLibrary[i].title} </p>
@@ -29,16 +30,16 @@ function displayBooks() {
               <button type="button" class="check">Mark as read </button>
             </p>
             <p>
-              <button type="button" class="delete" onclick ="delete_book(${myLibrary[i].id})">Delete This Book</button>
+              <button type="button" class="delete" onclick ="deleteBook(${myLibrary[i].id})">Delete This Book</button>
             </p>
           </article>
           `;
-    }
+  }
 
-    document.getElementById('card').innerHTML = e;
+  document.getElementById('card').innerHTML = e;
 }
 
-function delete_book(id) {
-    myLibrary = myLibrary.filter((book) => book.id !== id);
-    displayBooks();
+function deleteBook(id) { // eslint-disable-line no-unused-vars
+  myLibrary = myLibrary.filter((book) => book.id !== id);
+  displayBooks(); // still raises defined but never used
 }
